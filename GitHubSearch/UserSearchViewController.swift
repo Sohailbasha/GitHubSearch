58384b9da340028700eb7b961b5195e9a4224d57
Sohailbasha <shlbsh94@gmail.com>
2020-05-31 19:53:48 -0500

    Create UserSearchViewController. 	* Is the initial VC 	* Set programmatically

---
+++ GitHubSearch/UserSearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserSearchViewController.swift
+//  GitHubSearch
+//
+//  Created by Ilias Basha on 5/31/20.
+//  Copyright © 2020 sohail. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+// MARK: - Table view data source
+extension UserSearchViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
