0a98838c659a3b28159ec0cff22f26e6de593740
Sohailbasha <shlbsh94@gmail.com>
2020-05-31 20:21:27 -0500

    IMPL functions to connect to API

--- GitHubSearch/APIService.swift
+++ GitHubSearch/APIService.swift
@@ -7,14 +7,54 @@
 //
 
 import Foundation
+import Combine
 
 class APIService {
 
-    let stringURL = "https://api.github.com/search/users" // for search
-    let userInfoURL = "https://api.github.com/users/" // for userInfo
-    
+    let userSearchBaseURL = "https://api.github.com/search/users" // for search
+    let userInfoBaseURL = "https://api.github.com/users/" // for userInfo
 
-    static func url(bbyAdding parameters: [String : String]?, to url: URL) -> URL {
+        func fetchUsers(query: String) -> AnyPublisher<[FetchedUser], Error> {
+            let parameters = ["q":query]
+
+            guard let baseURL = URL(string: userSearchBaseURL) else {
+                fatalError("Invalid URL")
+            }
+
+            let url = self.url(byAdding: parameters, to: baseURL)
+
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .receive(on: RunLoop.main)
+                .map { $0.data }
+                .decode(type: SearchResponse.self, decoder: JSONDecoder())
+                .map{ $0.items }
+                .eraseToAnyPublisher()
+        }
+
+        func getData(with user: FetchedUser) -> AnyPublisher<User, Error> {
+            let userInfoUrl = userInfoBaseURL + user.login
+            guard let url = URL(string: userInfoUrl) else { fatalError("Wrong URL") }
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue("Bearer 12286d850bdb4755ad2502f43b972a5331e1f797", forHTTPHeaderField: "Authorization")
+
+            return URLSession.shared.dataTaskPublisher(for: request)
+                .receive(on: RunLoop.main)
+                .map{$0.data}
+                .decode(type: User.self, decoder: JSONDecoder())
+                .eraseToAnyPublisher()
+        }
+
+        func getAllData(query: String) -> AnyPublisher<[User], Error> {
+            fetchUsers(query: query).flatMap { fetchedUsers in
+                Publishers.Sequence(sequence: fetchedUsers.map { self.getData(with: $0) })
+                    .flatMap{$0}
+                    .collect()
+
+            }.eraseToAnyPublisher()
+        }
+
+    func url(byAdding parameters: [String : String]?, to url: URL) -> URL {
         var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
         components?.queryItems = parameters?.compactMap({URLQueryItem(name: $0.0, value: $0.1)})
         
